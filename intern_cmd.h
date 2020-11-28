4708b6c16d1c7e7ca89a02ce97fe9fac76fbb5a2
MrChasseur <quentin38590@gmail.com>
2020-11-28 16:22:06 +0100

    ifndef

--- intern_cmd.h
+++ intern_cmd.h
@@ -1,5 +1,12 @@
+#ifndef __INTERN_CMD__H__
+#define __INTERN_CMD__H__
+
+
 int pwd(char **envp, char **args);
 
 char *path_parsing(char *path, char **envp, int pwd_loc, int home_loc);
 
 int cd(char **envp, char **args, int indice_pwd, int indice_home);
+
+
+#endif
